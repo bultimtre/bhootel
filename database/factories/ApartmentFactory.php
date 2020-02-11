45d00f9d9ee788ebd673210444bf0915146541a4
flaviocomi <flabio.co91@gmail.com>
2020-02-11 15:32:49 +0100

    Co-authored-by: GiuseppeVigliaturo <vigliaturo94@gmail.com> Co-authored-by: Simone Ugazio <simoneugazio1993@gmail.com>

--- database/factories/ApartmentFactory.php
+++ database/factories/ApartmentFactory.php
@@ -5,17 +5,8 @@
 use App\User;
 use Faker\Generator as Faker;
 use Illuminate\Support\Str;
+use App\Apartment;
 
-/*
-|--------------------------------------------------------------------------
-| Model Factories
-|--------------------------------------------------------------------------
-|
-| This directory should contain each of the model factory definitions for
-| your application. Factories provide a convenient way to generate new
-| model instances for testing / seeding your application's database.
-| Fabrizio c'è!
-*/
 
 $factory->define(Apartment::class, function (Faker $faker) {
     return [
