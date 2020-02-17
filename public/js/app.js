c0b0a459bb2a044e762d467a1306c5b4af7419ac
flaviocomi <flabio.co91@gmail.com>
2020-02-17 12:11:44 +0100

    ok

--- public/js/app.js
+++ public/js/app.js
@@ -52278,13 +52278,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-<<<<<<< HEAD
 __webpack_require__(/*! C:\Users\Flavio\Desktop\bhootel\resources\js\app.js */"./resources/js/app.js");
 module.exports = __webpack_require__(/*! C:\Users\Flavio\Desktop\bhootel\resources\sass\app.scss */"./resources/sass/app.scss");
-=======
-__webpack_require__(/*! C:\Boolean\bhootel\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\Boolean\bhootel\resources\sass\app.scss */"./resources/sass/app.scss");
->>>>>>> f4309f2c54ce4464e7b13e42da5e6aa5b3694363
 
 
 /***/ })
