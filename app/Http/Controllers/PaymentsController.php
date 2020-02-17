875040d01392ff8f7c5fd8908c9b6682a272f4d0
giuviglia94 <vigliaturo94@gmail.com>
2020-02-17 22:34:32 +0100

    braintree prova

---
+++ app/Http/Controllers/PaymentsController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Braintree_Transaction;
+use App\Apartment;
+class PaymentsController extends Controller
+{
+    public function make(Request $request)
+{
+    //dd($request);
+    $payload = $request->input('payload', false);
+    $nonce = $payload['nonce'];
+    $apartments= Apartment::all();
+    dd($apartments);
+    $status = Braintree_Transaction::sale([
+                            'amount' => '12.00',
+                            'paymentMethodNonce' => $nonce,
+                            'options' => [
+                                       'submitForSettlement' => True
+                                         ]
+              ]);
+              
+    return response()->json($status);
+   // return view('hosted',compact('status'));
+}
+}
--- routes/web.php
+++ routes/web.php
@@ -6,3 +6,11 @@ Route::get('/', 'GuestController@index')-> name('guest.home');
 Route::post('/search', 'GuestController@search')-> name('guest.search');
 
 Route::get('/apartment/{id}', 'GuestController@show')-> name('apartment.show');
+
+
+//braintree
+Route::view('/dropin','drop-ui');
+Route::view('/hosted','hosted');
+//Route::get('/payment/make', 'PaymentsController@make')->name('payment.make');
+Route::post('/payment/make', 'PaymentsController@make')->name('payment.make');
+
