508855c0924389a2fc5b44d5df73b1206ba670cb
flaviocomi <flabio.co91@gmail.com>
2020-02-21 11:40:56 +0100

    header sbagliato

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -11,43 +11,45 @@
                         <img src="{{ url('/') }}/images/BhootelLogo.png" alt="">
                     </a>
                 </li>
-            @if(!Route::is('bhootel.login') && !Route::is('register'))
-                {{-- GUEST login + register--}}
-                @if(Auth::guest())
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{ route('bhootel.login','upr') }}">Login</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{ route('register') }}">Register</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{ route ('bhootel.login','apt') }}">Inserisci appartamento</a>
-                    </li>
-                {{-- GUEST logout + name user--}}
-                @elseif(Auth::user())
-                    <li class="nav-item">
-                        <a class="nav-link user-name" href="{{ route('user.user-panel') }}"><i class="fas fa-user    "></i>{{Auth::user()->name}}</a>
-                    </li>
-                    <li class="d-flex">
-                        <div class="nav-item">
-                            <a class="nav-link" href="{{ route ('user-apt.create') }}">Inserisci appartamento</a>
-                        </div>
-                        <div class="nav-item">
-                            <a class="nav-link" href="{{ route('logout') }}"
-                            onclick="event.preventDefault();
-                                        document.getElementById('logout-form').submit();">
-                            {{ __('Logout') }}
-                            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                @csrf
-                                @method('post')
-                            </form>
-                            </a>
-                        </div>
-                    </li>
+                @if(!Route::is('bhootel.login') && !Route::is('register'))
+                    {{-- GUEST login + register--}}
+                    @if(Auth::guest())
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('bhootel.login','upr') }}">Login</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('register') }}">Register</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route ('bhootel.login','apt') }}">Inserisci appartamento</a>
+                        </li>
+                    {{-- GUEST logout + name user--}}
+                    @elseif(Auth::user())
+                        <li class="nav-item">
+                            <a class="nav-link user-name" href="{{ route('user.user-panel') }}"><i class="fas fa-user    "></i>{{Auth::user()->name}}</a>
+                        </li>
+                        <li class="d-flex">
+                            <div class="nav-item">
+                                <a class="nav-link" href="{{ route ('user-apt.create') }}">Inserisci appartamento</a>
+                            </div>
+                            <div class="nav-item">
+                                <a class="nav-link" href="{{ route('logout') }}"
+                                onclick="event.preventDefault();
+                                            document.getElementById('logout-form').submit();">
+                                {{ __('Logout') }}
+                                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                    @csrf
+                                    @method('post')
+                                </form>
+                                </a>
+                            </div>
+                        </li>
+                    @endif
+                    {{-- ADD APT GUEST/UPR/UPRA --}}
+                    
                 @endif
-                {{-- ADD APT GUEST/UPR/UPRA --}}
-
-        </ul>
+            </ul>
+        </div>
        {{-- GUEST/UPR/UPRA --}}
         {{-- SOSTITUITA DA CANC <form class="form-inline mt-2 mt-md-0" action="{{route(Auth::user()?'user.search':'guest.search')}}" method="post"> --}}
         <form class="form-inline mt-2 mt-md-0" action="{{route('search.show')}}" method="post">
@@ -55,8 +57,8 @@
             @method('POST')
             <label for="search_field"></label>
             <input class="form-control mr-sm-2" name='search_field' type="text">
-
-
+        </form>
+    </nav>
 
 
 <script>
