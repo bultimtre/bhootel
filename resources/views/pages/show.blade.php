c07fb341239195999c1f875a63417f93574a27ef
flaviocomi <flabio.co91@gmail.com>
2020-02-26 16:08:58 +0100

    tests

--- resources/views/pages/show.blade.php
+++ resources/views/pages/show.blade.php
@@ -87,29 +87,38 @@
     </div>
 </div>
 
-<a href="{{route('charts', $apartment -> id)}}">vedi stats</a>
+@auth
+    @if (Auth::user() -> id == $apartment -> user -> id)
+        <a href="{{route('charts', $apartment -> id)}}">vedi stats</a>
+    @endif
+@endauth
 
 @auth
-@if (Auth::user() -> id != $apartment -> user -> id)
+    @if (Auth::user() -> id != $apartment -> user -> id)
 
-<form class="mt-5" id="uploadForm" method="POST" action="{{route('mail-store')}}" enctype="multipart/form-data">
-    @csrf
-    @method('POST')
-    
-    <div class="form-group{{ $errors->has('description') ? ' has-error' : '' }} formField">
-        <label for="comment">Contatta il proprietario</label>
-        <input type="hidden" name="id" value=" {{$apartment -> user -> id}} ">
-        <input type="hidden" name="id-apt" value=" {{$apartment -> id}} ">
-        <textarea class="form-control" rows="5" name="text" maxlength="750"></textarea>
-    </div>
-    
-    <div class="form-group">
-        <button type="submit" name="button" class="btn btn-primary">Invia</button>
-    </div>
-    
-</form>
+        <form class="mt-5" id="uploadForm" method="POST" action="{{route('mail-store')}}" enctype="multipart/form-data">
+            @csrf
+            @method('POST')
 
-@endif
+            <div class="form-group">
+                <label for="email" class="col-md-4 col-form-label">Indirizzo Email</label>
+                <input type="email" class="form-control  @error('email') is-invalid @enderror" id="email" aria-describedby="emailHelp" name="email" value="{{ Auth::user() -> email }}" required autocomplete="off">
+            </div>
+            
+            <div class="form-group{{ $errors->has('description') ? ' has-error' : '' }} formField">
+                <label for="comment">Contatta il proprietario</label>
+                <input type="hidden" name="id" value=" {{$apartment -> user -> id}} ">
+                <input type="hidden" name="id-apt" value=" {{$apartment -> id}} ">
+                <textarea class="form-control" rows="5" name="text" minlength="1" maxlength="750" required></textarea>
+            </div>
+            
+            <div class="form-group">
+                <button type="submit" name="button" class="btn btn-primary">Invia</button>
+            </div>
+            
+        </form>
+
+    @endif
 @endauth
 
 
@@ -117,6 +126,11 @@
 <form class="mt-5" id="uploadForm" method="POST" action="{{route('mail-store')}}" enctype="multipart/form-data">
     @csrf
     @method('POST')
+
+    <div class="form-group">
+        <label for="email" class="col-md-4 col-form-label">Indirizzo Email</label>
+        <input type="email" class="form-control  @error('email') is-invalid @enderror" id="email" aria-describedby="emailHelp" placeholder="Inserisci email" name="email" value="{{ old('email') }}" required autocomplete="email">
+    </div>
     
     <div class="form-group{{ $errors->has('description') ? ' has-error' : '' }} formField">
         <label for="comment">Contatta il proprietario</label>
